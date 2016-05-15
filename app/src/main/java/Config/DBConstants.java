cc531ba41a1d18d27fa53015d6c0a1ecb4d2ff21
AbelN <abelngaliema01@gmail.com>
2016-05-15 15:15:47 +0200

    Updated

---
+++ app/src/main/java/Config/DBConstants.java
@@ -0,0 +1,9 @@
+package Config;
+
+/**
+ * Created by AbelN on 20/04/2016.
+ */
+public class DBConstants {
+    public static final String DATABASE_NAME = "myDBV";
+    public static final int DATABASE_VERSION = 6;
+}
---
+++ app/src/main/java/Domain/CustomerInterface.java
@@ -0,0 +1,11 @@
+package Domain;
+
+/**
+ * Created by AbelN on 15/04/2016.
+ */
+public interface CustomerInterface {
+
+    Long getId();
+    CustomerAddress getCustomerAddress();
+    PersonalInformation getPersonalInformation();
+}
